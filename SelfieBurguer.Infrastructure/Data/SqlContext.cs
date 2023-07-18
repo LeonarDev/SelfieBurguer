300100c9c0cf78d047489a30b2ffe03e6e153596
LeonarDev <leonardo.majevski@gmail.com>
2023-07-17 23:28:34 -0300

    05) Cria DbContext

---
+++ SelfieBurguer.Infrastructure/Data/SqlContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using SelfieBurguer.Domain.Entities;
+
+namespace SelfieBurguer.Infrastructure.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext() { }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public override int SaveChanges()
+        {
+            foreach (EntityEntry? entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
