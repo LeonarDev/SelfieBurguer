f30485aa67e0559afee89be36b123e357d3c67af
LeonarDev <leonardo.majevski@gmail.com>
2023-09-01 01:17:33 -0300

    Adiciona configuração Kubernets

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,15 +13,6 @@ echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo  
 sleep $wait_time
 
-# run scripts in /schemas folder to create the DB and tables
-# for entry in "schemas/*.sql"
-# do
-#     echo EXECUTANDO $entry
-#     # /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i $entry
-#     /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "use master"
-#     /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "create table teste(Id INT NOT NULL IDENTITY PRIMARY KEY, Nome varchar(255) NOT NULL)"
-# done
-
 echo EXECUTANDO SCRIPTS...
 
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P YourStrong@Passw0rd -d master -i ./init.sql
