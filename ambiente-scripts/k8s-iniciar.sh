221e139984c441a9142d6045da2d7ea0d42bcc14
LeonarDev <leonardo.majevski@gmail.com>
2023-09-04 18:49:44 -0300

    Ajusta shel script

--- ambiente-scripts/k8s-iniciar.sh
+++ ambiente-scripts/k8s-iniciar.sh
@@ -9,36 +9,36 @@ echo ..
 echo .
 echo 
 
-sleep 3;
 
 echo .
 echo ..
 echo ...
 echo ...CRIANDO SERVICE DO BANCO:
+sleep 3;
 kubectl apply -f k8s/mssql-service.yaml;
 echo ...
 echo ..
 echo .
 echo 
 
-sleep 3;
 
 echo .
 echo ..
 echo ...
 echo ...CRIANDO DEPLOYMENT DA APLICACAO:
+sleep 3;
 kubectl apply -f k8s/app-deployment.yaml;
 echo ...
 echo ..
 echo .
 echo 
 
-sleep 3;
 
 echo .
 echo ..
 echo ...
 echo ...CRIANDO SERVICE DA APLICACAO:
+sleep 3;
 kubectl apply -f k8s/app-service.yaml
 echo ...
 echo ..
